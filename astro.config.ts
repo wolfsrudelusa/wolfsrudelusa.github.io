2698703386230553558a834c4269d36734af0ce9
Lex <58815784+avonbied@users.noreply.github.com>
2025-10-27 13:35:59 -0500

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import pwa from '@vite-pwa/astro';
+
+import PROJECT_CONFIG from './project.config.ts';
+
+// https://astro.build/config
+export default defineConfig({
+	site: `${PROJECT_CONFIG.site}:${PROJECT_CONFIG.port}`,
+	base: PROJECT_CONFIG.base,
+	integrations: [sitemap(), pwa()],
+	server: {
+		port: PROJECT_CONFIG.port,
+		host: true,
+	},
+	vite: {
+		server: {
+			watch: {
+				usePolling: true,
+				interval: 10000
+			}
+		}
+	}
+});
---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+import process from 'node:process';
+
+import PROJECT_CONFIG from './project.config.ts';
+
+export default defineConfig({
+	testDir: 'test',
+	fullyParallel: true,
+	use: {
+		baseURL: PROJECT_CONFIG.fullSite(true),
+	},
+	projects: [
+		{
+			name: 'chrome',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'safari',
+			use: { ...devices['Desktop Safari'] }
+		}
+	],
+	webServer: {
+		command: 'yarn run preview',
+		url: PROJECT_CONFIG.fullSite(true),
+		timeout: 120 * 1000,
+		reuseExistingServer: !process.env.CI
+	}
+});
